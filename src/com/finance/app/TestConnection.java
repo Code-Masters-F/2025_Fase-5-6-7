c7094dec7c493cf804beb9f1aa0553d94467c838
Johnvitordev <joaovitor.dev14@gmail.com>
2025-08-25 20:14:14 -0300

    feat: adiconando a conexão com o banco de dados FIAP

---
+++ src/com/finance/app/TestConnection.java
@@ -0,0 +1,23 @@
+package com.finance.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        try {
+
+            String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
+            String user = "RM557577";
+            String password = "140206";
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("✅ Conexão estabelecida com sucesso!");
+
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("❌ Falha ao conectar ao banco de dados. Verifique as credenciais ou a URL.");
+            e.printStackTrace();
+        }
+    }
+}
