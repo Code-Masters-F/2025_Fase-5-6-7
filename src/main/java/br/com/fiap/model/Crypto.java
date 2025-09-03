f93aae0217fb140bc60d79c0c34f99278dcc1811
Guilherme-Avellar <guilhermeavellar25@gmail.com>
2025-09-03 12:02:43 -0300

    chore: mantendo construtor para aceitar String em data

--- src/main/java/br/com/fiap/model/Crypto.java
+++ src/main/java/br/com/fiap/model/Crypto.java
@@ -11,6 +11,13 @@ public class Crypto {
     private LocalDate dataLancamento;
 
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public Crypto(String nome, String sigla, int id, String dataLancamento) {
+        this.nome = nome;
+        this.sigla = sigla;
+        this.dataLancamento = LocalDate.parse(dataLancamento, formatter);
+        this.id = id;
+    }
+
     public Crypto(String nome, String sigla, int id, LocalDate dataLancamento) {
         this.nome = nome;
         this.sigla = sigla;
